80432cb648dc4d1dc560f1b1c56307dd90f0ea25
wooaktkfn <62277609+wooaktkfn@users.noreply.github.com>
2020-05-14 01:36:00 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,1 +1,9 @@
-<img src="https://user-images.githubusercontent.com/62277609/81839029-f3185780-9581-11ea-8632-157ef7bae709.png">
+<!doctype html>
+<html><head><meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<title>돔</title>
+</head>
+<body>
+<img src="https://user-images.githubusercontent.com/62277609/81839029-f3185780-9581-11ea-8632-157ef7bae709.png" border=0 usemap="#area">
+   </body>
+</html>
